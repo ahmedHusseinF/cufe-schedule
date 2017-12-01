c5af7c233a6357fe258b53c753ab4fd38e83bdd9
ahmedHusseinF <hussein.ahmed50@gmail.com>
2017-12-02 01:46:58 +0200

    finished the interference of lecs and tuts

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express()
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`app started on port ${port}`)
+});
